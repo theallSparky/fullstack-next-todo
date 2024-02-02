2cb926cb113d3614cbc500bbd3142258c21d03ad
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-02 00:11:30 -0600

    Edited page.js x3

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>We are on the dashboard!</h1>
+    </div>
+  );
 };
 
 export default page;
---
+++ app/greeting/name/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <h1>Very pleased to eet you!</h1>;
+};
+
+export default page;
