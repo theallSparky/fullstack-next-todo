11a8bfdf56bf9f4c9cbf3dc5c6fbb5d0f20116b5
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-05 16:46:28 -0600

    added 1 picture, imported in page file, made 2 copies of pic on page

--- app/images/page.js
+++ app/images/page.js
@@ -1,7 +1,15 @@
 import React from "react";
+import hike_w_family from "../../assets/hike_w_family.jpeg";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      We will show the image here
+      <Image src={hike_w_family} alt="nature" />
+      <Image src={hike_w_family} alt="nature" width={500} />
+    </div>
+  );
 };
 
 export default page;
