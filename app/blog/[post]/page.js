d595433e49942efe1105febc5b3abfad6a166f7a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-05 16:48:02 -0600

    Deleted files

--- app/blog/[post]/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = ({ params }) => {
-  return <h1>Post : {params.post}</h1>;
-};
-
-export default page;
--- app/greeting/[name]/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = ({ params }) => {
-  return <h1>Very pleased to meet you, {params.name}!</h1>;
-};
-
-export default page;
