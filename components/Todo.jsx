4cf99296f454b9f4906a8ebec95a0d1968806472
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-06 17:38:26 -0600

    Staged todo component changes

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <div>
+      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <th
+          scope="row"
+          class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+          Apple MacBook Pro 17&quot;
+        </th>
+        <td class="px-6 py-4">Silver</td>
+        <td class="px-6 py-4">Laptop</td>
+        <td class="px-6 py-4">$2999</td>
+      </tr>
+    </div>
+  );
 };
 
 export default Todo;
