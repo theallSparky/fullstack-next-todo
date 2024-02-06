6f5dfc1580c6700bb8c8a856c73ee97a88085dcf
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-06 17:31:22 -0600

    Switched a few labels from preset

--- app/page.js
+++ app/page.js
@@ -18,21 +18,24 @@ export default function Home() {
         </button>
       </form>
 
-      <div className="relative overflow-x-auto">
+      <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                Id
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
+                Title
               </th>
               <th scope="col" className="px-6 py-3">
-                Category
+                Description
               </th>
               <th scope="col" className="px-6 py-3">
-                Price
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
               </th>
             </tr>
           </thead>
@@ -58,17 +61,6 @@ export default function Home() {
               <td className="px-6 py-4">White</td>
               <td className="px-6 py-4">Laptop PC</td>
               <td className="px-6 py-4">$1999</td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
             </tr>
           </tbody>
         </table>
