8c8f84e7f4ecd062197ca53fd219a97ed323df23
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-06 16:37:16 -0600

    Fixed typos

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flexpy-3 flex-wrapjustify-around">
-      <h1 className="text-lgfont-semibold">Todo App</h1>
-      <ul className="flex-gap-[40px]text-m">
-        <li>Hime</li>
+    <div className="flex py-3 flex-wrap justify-around">
+      <h1 className="text-lg font-semibold">Todo App</h1>
+      <ul className="flex-gap-[40px] text-m">
+        <li>Home</li>
         <li>Products</li>
         <li>About</li>
         <li>Contact</li>
