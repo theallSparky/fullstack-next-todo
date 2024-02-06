613ffde904aeeb610c1a125d7db89a76c18f7040
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-06 16:51:13 -0600

    Fixed navbar not displaying properly

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="flex py-3 flex-wrap justify-around">
       <h1 className="text-lg font-semibold">Todo App</h1>
-      <ul className="flex-gap-[40px] text-m">
+      <ul className="flex gap-[40px] text-m">
         <li>Home</li>
         <li>Products</li>
         <li>About</li>
