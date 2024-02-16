919088439647ca37cfc94c5f17279c5679441be6
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-02-15 18:02:09 -0600

    Changed class to className, in Todo.jsx, reverted page.js to catch bug easier

--- app/page.js
+++ app/page.js
@@ -25,24 +25,53 @@ export default function Home() {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Id
+                Product name
               </th>
               <th scope="col" className="px-6 py-3">
-                Title
+                Color
               </th>
               <th scope="col" className="px-6 py-3">
-                Description
+                Category
               </th>
               <th scope="col" className="px-6 py-3">
-                Status
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
+                Price
               </th>
             </tr>
           </thead>
           <tbody>
-            <Todo />
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17&quot;
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Microsoft Surface Pro
+              </th>
+              <td className="px-6 py-4">White</td>
+              <td className="px-6 py-4">Laptop PC</td>
+              <td className="px-6 py-4">$1999</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Magic Mouse 2
+              </th>
+              <td className="px-6 py-4">Black</td>
+              <td className="px-6 py-4">Accessories</td>
+              <td className="px-6 py-4">$99</td>
+            </tr>
           </tbody>
         </table>
       </div>
--- components/Todo.jsx
+++ components/Todo.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Todo = () => {
   return (
     <div>
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
         <th
           scope="row"
-          class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
         >
           1
         </th>
-        <td class="px-6 py-4">Study</td>
-        <td class="px-6 py-4">Learn a code concept</td>
-        <td class="px-6 py-4">Pending...</td>
-        <td class="px-6 py-4">
+        <td className="px-6 py-4">Study</td>
+        <td className="px-6 py-4">Learn a code concept</td>
+        <td className="px-6 py-4">Pending...</td>
+        <td className="px-6 py-4">
           <button className="py-2 px-4 bg-red-500 text-white">Delete</button>
         </td>
       </tr>
