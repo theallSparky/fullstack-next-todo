8d10bde9ef5d318d93a98e704a7533db66805a12
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-05 19:44:37 -0600

    Added Delete and Done buttons, added gap between them too

--- app/page.js
+++ app/page.js
@@ -42,17 +42,7 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple MacBook Pro 17&quot;
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-            </tr>
+            <Todo></Todo>
           </tbody>
         </table>
       </div>
--- components/Todo.jsx
+++ components/Todo.jsx
@@ -2,22 +2,21 @@ import React from "react";
 
 const Todo = () => {
   return (
-    <div>
-      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-        >
-          1
-        </th>
-        <td className="px-6 py-4">Study</td>
-        <td className="px-6 py-4">Learn a code concept</td>
-        <td className="px-6 py-4">Pending...</td>
-        <td className="px-6 py-4">
-          <button className="py-2 px-4 bg-red-500 text-white">Delete</button>
-        </td>
-      </tr>
-    </div>
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        1
+      </th>
+      <td className="px-6 py-4">Study</td>
+      <td className="px-6 py-4">Learn Next JS</td>
+      <td className="px-6 py-4">Pending...</td>
+      <td className="px-6 py-4 flex gap-1">
+        <button className="py-2 px-4 bg-red-500 text-white">Delete</button>
+        <button className="py-2 px-4 bg-green-500 text-black">Done</button>
+      </td>
+    </tr>
   );
 };
 
