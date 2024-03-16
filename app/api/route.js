29c657854de5ca1f0e8b15cc9403e0aa38114b9d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-16 13:38:09 -0500

    More backend work completed

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
+const LoadDB = async () => {
+  await ConnectDB();
+};
+
 export async function GET(request) {
   return NextResponse.json({ msg: "get method hit" });
 }
