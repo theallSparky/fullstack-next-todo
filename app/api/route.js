d2824ae8fd8e4cfda5d808d109759fa37f60dfab
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-17 22:40:00 -0500

    Worked on post request

--- app/api/route.js
+++ app/api/route.js
@@ -7,3 +7,14 @@ const LoadDB = async () => {
 export async function GET(request) {
   return NextResponse.json({ msg: "get method hit" });
 }
+
+export async function POST(request) {
+  const { title, description } = await request.json();
+
+  await TodoModel.create({
+    title,
+    description,
+  });
+
+  return NextResponse.json({ msg: "Todo created!" });
+}
