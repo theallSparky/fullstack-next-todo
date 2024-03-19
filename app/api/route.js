0ed63fc9de1c48df5425a8236a6390bf53811317
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-19 17:47:07 -0500

    Made a put request for updating todos as complete

--- app/api/route.js
+++ app/api/route.js
@@ -29,3 +29,13 @@ export async function DELETE(request) {
   await TodoModel.findByIdAndDelete(mongoId);
   return NextResponse.json({ msg: "Todo deleted!" });
 }
+
+export async function PUT(request) {
+  const mongoId = await request.nextUrl.searchParams.get("mongoId");
+  await TodoModel.findByIdAndUpdate(mongoId, {
+    $set: {
+      isCompleted: true,
+    },
+  });
+  return NextResponse.json({ msg: "Todo completed!" });
+}
