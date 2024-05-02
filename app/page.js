87ebd0e8a4e1ee295509e042d78d3d82ca9f90fe
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-05-02 00:38:06 -0500

    Added a particle background

--- app/page.js
+++ app/page.js
@@ -73,11 +73,10 @@ export default function Home() {
   return (
     <>
       <ToastContainer theme="dark" closeOnClick autoClose="3500" />
-      <ParticlesComponent id="particles" />
 
       <form
         onSubmit={onSubmitHandler}
-        className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto"
+        className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 p-2 mx-auto bg-red-500 border rounded-lg hover:bg-red-700 duration-200 ease-in"
       >
         <input
           onChange={onChangeHandler}
@@ -94,13 +93,17 @@ export default function Home() {
           placeholder="Enter Description"
           className="px-3 py-2 border-2 w-full"
         ></textarea>
-        <button type="submit" className="bg-orange-600 py-3 px-11 text-white">
+        <button
+          type="submit"
+          className="bg-orange-600 py-3 px-11 text-white self-center"
+        >
           Add ToDo!
         </button>
       </form>
+      <ParticlesComponent id="particles" />
 
       <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mb-5">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
