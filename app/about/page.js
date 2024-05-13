7b64d64b8a4baac9526c1544ac7eb1eb66ccd7c0
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-05-13 01:36:04 -0500

    added a new page linking to about and added the same bg as the home page

---
+++ app/about/page.js
@@ -0,0 +1,6 @@
+"use client";
+import ParticlesComponent from "@/components/particles";
+
+export default function About() {
+  return <ParticlesComponent id="particles" />;
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="flex py-3 flex-wrap justify-around">
       <h1 className="text-lg font-semibold">Todo App</h1>
       <ul className="flex gap-[40px] text-m">
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="">
+          <li>Products</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="">
+          <li>Contact</li>
+        </Link>
       </ul>
     </div>
   );
