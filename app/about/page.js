dac8eb62e3cdf381cdd5a2f6c7f0a5e171c9036c
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-05-13 02:54:20 -0500

    Made a small little basic greeting for the about page

--- app/about/page.js
+++ app/about/page.js
@@ -2,5 +2,10 @@
 import ParticlesComponent from "@/components/particles";
 
 export default function About() {
-  return <ParticlesComponent id="particles" />;
+  return (
+    <>
+      <ParticlesComponent id="particles" />
+      <h2>Here is the about page!</h2>
+    </>
+  );
 }
