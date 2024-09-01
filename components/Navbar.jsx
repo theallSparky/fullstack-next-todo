a11b9c3d4e747114e2751b10073cb041725a5d64
Jonathan Streetman <jonathanstreetman@Jonathans-MacBook-Air-2.local>
2024-08-31 20:24:07 -0400

    landing page markup

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,256 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { FiMenu, FiArrowRight, FiX, FiChevronDown } from "react-icons/fi";
+import { FaUserCircle, FaBook } from "react-icons/fa";
+import {
+  useMotionValueEvent,
+  AnimatePresence,
+  useScroll,
+  motion,
+} from "framer-motion";
+import useMeasure from "react-use-measure";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="flex py-3 flex-wrap justify-around">
-      <h1 className="text-lg font-semibold">Todo App</h1>
-      <ul className="flex gap-[40px] text-m">
-        <Link href="/">
-          <li>Home</li>
-        </Link>
-        <Link href="">
-          <li>Products</li>
-        </Link>
-        <Link href="/about">
-          <li>About</li>
-        </Link>
-        <Link href="">
-          <li>Contact</li>
-        </Link>
-      </ul>
+    <>
+      <FlyoutNav />
+      <div className="relative min-h-screen">
+        <div className="absolute inset-0 z-0 bg-gradient-to-b from-neutral-950/90 to-neutral-950/0" />
+      </div>
+    </>
+  );
+};
+
+const FlyoutNav = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setScrolled(latest > 250 ? true : false);
+  });
+
+  return (
+    <nav
+      className={`fixed top-0 z-50 w-full px-6 text-white 
+      transition-all duration-300 ease-out lg:px-12
+      ${
+        scrolled
+          ? "bg-neutral-950 py-3 shadow-xl"
+          : "bg-neutral-950/0 py-6 shadow-none"
+      }`}
+    >
+      <div className="mx-auto flex max-w-7xl items-center justify-between">
+        <Logo />
+        <div className="hidden gap-6 lg:flex">
+          <Links />
+          <CTAs />
+        </div>
+        <MobileMenu />
+      </div>
+    </nav>
+  );
+};
+
+const Logo = ({ color = "white" }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <span className="text-2xl font-bold" style={{ color }}>
+        Todo
+      </span>
+      <FaBook />
+    </div>
+  );
+};
+
+const Links = () => {
+  return (
+    <div className="flex items-center gap-6">
+      {LINKS.map((l) => (
+        <NavLink key={l.text} href={l.href} FlyoutContent={l.component}>
+          {l.text}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+const NavLink = ({ children, href, FlyoutContent }) => {
+  const [open, setOpen] = useState(false);
+
+  const showFlyout = FlyoutContent && open;
+
+  return (
+    <div
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className="relative h-fit w-fit"
+    >
+      <a href={href} className="relative">
+        {children}
+        <span
+          style={{
+            transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
+          }}
+          className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out"
+        />
+      </a>
+      <AnimatePresence>
+        {showFlyout && (
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 15 }}
+            style={{ translateX: "-50%" }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="absolute left-1/2 top-12 bg-white text-black"
+          >
+            <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
+            <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" />
+            <FlyoutContent />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+const CTAs = () => {
+  return (
+    <div className="flex items-center gap-3">
+      <Link
+        href="/signin"
+        className="flex items-center gap-2 rounded-lg border-2 border-white px-4 py-2 font-semibold text-white transition-colors hover:bg-white hover:text-black"
+      >
+        <FaUserCircle />
+        <span>Sign In</span>
+      </Link>
+      <Link
+        href="/signup"
+        className="rounded-lg border-2 border-blue-500 bg-blue-500 px-4 py-2 font-semibold text-black transition-colors hover:border-blue-600 hover:bg-blue-600 hover:text-white"
+      >
+        Sign Up
+      </Link>
+    </div>
+  );
+};
+
+const MobileMenuLink = ({ children, href, FoldContent, setMenuOpen }) => {
+  const [ref, { height }] = useMeasure();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="relative text-neutral-950">
+      {FoldContent ? (
+        <div
+          className="flex w-full cursor-pointer items-center justify-between border-b border-neutral-300 py-6 text-start text-2xl font-semibold"
+          onClick={() => setOpen((pv) => !pv)}
+        >
+          <a
+            onClick={(e) => {
+              e.stopPropagation();
+              setMenuOpen(false);
+            }}
+            href={href}
+          >
+            {children}
+          </a>
+          <motion.div
+            animate={{ rotate: open ? "180deg" : "0deg" }}
+            transition={{
+              duration: 0.3,
+              ease: "easeOut",
+            }}
+          >
+            <FiChevronDown />
+          </motion.div>
+        </div>
+      ) : (
+        <a
+          onClick={(e) => {
+            e.stopPropagation();
+            setMenuOpen(false);
+          }}
+          href="#"
+          className="flex w-full cursor-pointer items-center justify-between border-b border-neutral-300 py-6 text-start text-2xl font-semibold"
+        >
+          <span>{children}</span>
+          <FiArrowRight />
+        </a>
+      )}
+      {FoldContent && (
+        <motion.div
+          initial={false}
+          animate={{
+            height: open ? height : "0px",
+            marginBottom: open ? "24px" : "0px",
+            marginTop: open ? "12px" : "0px",
+          }}
+          className="overflow-hidden"
+        >
+          <div ref={ref}>
+            <FoldContent />
+          </div>
+        </motion.div>
+      )}
+    </div>
+  );
+};
+
+const MobileMenu = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="block lg:hidden">
+      <button onClick={() => setOpen(true)} className="block text-3xl">
+        <FiMenu />
+      </button>
+      <AnimatePresence>
+        {open && (
+          <motion.nav
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100vw" }}
+            transition={{ duration: 0.15, ease: "easeOut" }}
+            className="fixed left-0 top-0 flex h-screen w-full flex-col bg-white"
+          >
+            <div className="flex items-center justify-between p-6">
+              <Logo color="black" />
+              <button onClick={() => setOpen(false)}>
+                <FiX className="text-3xl text-neutral-950" />
+              </button>
+            </div>
+            <div className="h-screen overflow-y-scroll bg-neutral-100 p-6">
+              {LINKS.map((l) => (
+                <MobileMenuLink
+                  key={l.text}
+                  href={l.href}
+                  FoldContent={l.component}
+                  setMenuOpen={setOpen}
+                >
+                  {l.text}
+                </MobileMenuLink>
+              ))}
+            </div>
+            <div className="flex justify-end bg-neutral-950 p-6">
+              <CTAs />
+            </div>
+          </motion.nav>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 
 export default Navbar;
+
+const LINKS = [
+  {
+    text: "About",
+    href: "#",
+  },
+  {
+    text: "Contact",
+    href: "#",
+  },
+];
--- contributions.md
+++ contributions.md
@@ -3,6 +3,7 @@
 I will contribute to this project in the following ways:
 
 - **Adding Auth**: Currently working on frontend auth code for /signup and /signin routes.
+- **Adding New Landing Page**: Currently working on frontend new landing page.
 - **Adding Animations**: Animations will be coming from framer-motion library.
 - **Updating Designs**: Some designs will be updated.
 
